758a90bf9a63bf597627b8f14901f193f6a5d318
Sven <sven@nimbostratus.de>
2025-10-03 16:06:03 +0200

    Changed the Row display to be a Grid

--- src/app/components/CardRow.tsx
+++ src/app/components/CardRow.tsx
@@ -1,28 +1,25 @@
-import { Card, Flex, Space, Text } from "@mantine/core";
-import VillageCard from "./VillageCard";
+import { Card, Grid, Space, Text } from "@mantine/core";
 import prisma from "../lib/prisma";
+import VillageCard from "./VillageCard";
 import CreateModal from "./CreateModal";
 
 export default async function CardRow() {
     const villages = await prisma.village.findMany();
 
     return (
-        <Flex gap="xs">
-            {
-                villages.length === 0 &&
-                <Flex justify="center" align="center" h="50vh" w="100%">
-                    <Card withBorder shadow="sm" radius="md" padding="lg">
-                        <Text>No villages found</Text>
-                        <Space h="md"></Space>
-                        <CreateModal />
-                    </Card>
-                </Flex>
-            }
-            {
-                villages.map((village, i) => (
-                    <VillageCard village={village} key={i} />
+        <Grid>
+            {villages.length === 0 ? (
+                <Card withBorder shadow="sm" radius="md" padding="lg">
+                    <Text>No villages found</Text>
+                    <Space h="md"></Space>
+                    <CreateModal />
+                </Card>
+            ) : (
+                villages.map((village) => (
+                    <VillageCard village={village} key={village.id} />
                 ))
+            )
             }
-        </Flex>
+        </Grid>
     );
 }
--- src/app/components/VillageCard.tsx
+++ src/app/components/VillageCard.tsx
@@ -1,16 +1,19 @@
-import { Button, Card, Space, Text } from "@mantine/core";
+"use client";
+import { Button, Card, Grid, Space, Text } from "@mantine/core";
 import { Village } from "@prisma/client";
 
-export default async function VillageCard({ village }: { village: Village }) {
+export default function VillageCard({ village }: { village: Village }) {
     return (
-        <Card shadow="sm" radius="md" padding="lg" withBorder>
-            <Text fw={500}>{village.name}</Text>
-            <Text size="sm" c="dimmed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-            </Text>
-            <Space h="md" />
-            <Button color="blue" fullWidth>Show</Button>
-        </Card>
+        <Grid.Col key={village.id} span={{ base: 12, md: 6, lg: 4 }}>
+            <Card shadow="sm" radius="md" padding="lg" withBorder>
+                <Text fw={500}>{village.name}</Text>
+                <Text size="sm" c="dimmed">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                </Text>
+                <Space h="md" />
+                <Button color="blue" fullWidth>Show</Button>
+            </Card>
+        </Grid.Col>
     )
 }
 
